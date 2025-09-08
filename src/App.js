d0a14d0a8015a90bc0219734fcadc9d51de1100e
PhusiHuynh1 <si_dth216127@student.agu.edu.vn>
2025-09-08 23:02:55 +0700

    refactor: restructure Layout components and update styles
    - Move and rename component folders to components/Layouts
    - Add SCSS modules for DefaultLayout, Sidebar, Header
    - Update imports in App.js, index.js, routes/index.js
    - Add logo and assets/images
    - Install packages: @fortawesome/free-solid-svg-icons, @fortawesome/react-fontawesome, classnames
    - Update GlobalStyles and layout components to use SCSS modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { pullicRoutes } from '~/routes';
-import DefaultLayout from '~/component/Layout/DefaultLayout';
+import DefaultLayout from '~/components/Layouts/DefaultLayout';
 
 function App() {
     return (
